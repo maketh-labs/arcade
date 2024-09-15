60e8bde8b3ab27af6e7388c73cea8b233b08e534
mansa maker <monsieur.mansa.musa@gmail.com>
2024-09-15 23:12:58 +0700

    dev: deploy giveaway policy

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -14,9 +14,12 @@ export default async function (hre: HardhatRuntimeEnvironment) {
   // Load contract
   const arcadeArtifact = await deployer.loadArtifact("Arcade");
   const mulRewardPolicyArtifact = await deployer.loadArtifact("MulRewardPolicy");
+  const giveawayPolicyArtifact = await deployer.loadArtifact("GiveawayPolicy");
   const arcadeContract = await deployer.deploy(arcadeArtifact, [vars.get("PROTOCOL_OWNER")]);
   const mulRewardPolicyContract = await deployer.deploy(mulRewardPolicyArtifact);
+  const giveawayPolicyContract = await deployer.deploy(giveawayPolicyArtifact);
 
   console.log(`${arcadeArtifact.contractName}: ${await arcadeContract.getAddress()}`);
   console.log(`${mulRewardPolicyArtifact.contractName}: ${await mulRewardPolicyContract.getAddress()}`);
+  console.log(`${giveawayPolicyArtifact.contractName}: ${await giveawayPolicyContract.getAddress()}`);
 }
--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -4,17 +4,20 @@ pragma solidity ^0.8.24;
 import {Script} from "forge-std/Script.sol";
 import {Arcade} from "../src/Arcade.sol";
 import {MulRewardPolicy} from "../src/MulRewardPolicy.sol";
+import {GiveawayPolicy} from "../src/GiveawayPolicy.sol";
 import {console} from "forge-std/console.sol";
 
 contract DeployScript is Script {
     function run() public {
         vm.startBroadcast(vm.envUint("DEPLOYER_PRIVATE_KEY"));
         address arcade = address(new Arcade(vm.envAddress("PROTOCOL_OWNER")));
-        address policy = address(new MulRewardPolicy());
+        address mulRewardPolicy = address(new MulRewardPolicy());
+        address giveawayPolicy = address(new GiveawayPolicy());
         vm.stopBroadcast();
 
         console.log("Arcade:", arcade);
-        console.log("Policy:", policy);
+        console.log("MulRewardPolicy:", mulRewardPolicy);
+        console.log("GiveawayPolicy:", giveawayPolicy);
         console.log("Owner:", vm.envAddress("PROTOCOL_OWNER"));
     }
 }
