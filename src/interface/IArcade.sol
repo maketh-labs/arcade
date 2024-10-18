b3cb1bbd935707eec34f6703132d7ce3e3ce149b
mansa maker <monsieur.mansa.musa@gmail.com>
2024-10-18 09:47:14 +0700

    dev: bump to 0.8.28

--- src/interface/IArcade.sol
+++ src/interface/IArcade.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24;
+pragma solidity ^0.8.28;
 
 interface IArcade {
     event Deposit(address user, address currency, uint256 amount);
--- src/interface/IRewardPolicy.sol
+++ src/interface/IRewardPolicy.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24;
+pragma solidity ^0.8.28;
 
 interface IRewardPolicy {
     function reward(uint256 toll, bytes calldata data) external returns (uint256 amount);
