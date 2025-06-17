2ebd17899af8c1fe0b301ec3420455ff7696a13d
mansa maker <monsieur.mansa.musa@gmail.com>
2025-06-17 20:41:19 +0900

    feat: create max min for ShootPolicy

--- src/ShootPolicy.sol
+++ src/ShootPolicy.sol
@@ -4,7 +4,11 @@ pragma solidity ^0.8.28;
 import {IRewardPolicy} from "./interfaces/IRewardPolicy.sol";
 
 contract ShootPolicy is IRewardPolicy {
-    function escrow(uint256, address, bytes calldata) external pure returns (uint256 amount) {
+    function escrow(uint256 toll, address, bytes calldata data) external pure returns (uint256 amount) {
+        (uint256 tollMinimum, uint256 tollMaximum) = abi.decode(data, (uint256, uint256));
+        if (toll < tollMinimum || toll > tollMaximum) {
+            revert("ShootPolicy: toll is out of range");
+        }
         return 0;
     }
 
